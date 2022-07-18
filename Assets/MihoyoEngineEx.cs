7b15ae0732a68cb4f0d0f9db7590272caa59daeb
ShiyumeMeguri <102138304+ShiyumeMeguri@users.noreply.github.com>
2022-07-18 14:23:23 +0900

    VCBaseMove,VelocityForceType

---
+++ Assets/MihoyoEngineEx.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public static class MihoyoEngineEx
+{
+    public static bool HasParameter(this Animator animator, int nameHash)
+    {
+        bool flag = false;
+        foreach (var para in animator.parameters)
+        {
+            if (para.nameHash == nameHash)
+            {
+                flag = true;
+                break;
+            }
+        }
+        return flag;
+    }
+}
