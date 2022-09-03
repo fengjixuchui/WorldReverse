b9ebb1ed95340a0da675f26d0feee53b890df1b3
ShiyumeMeguri <102138304+ShiyumeMeguri@users.noreply.github.com>
2022-09-03 17:12:29 +0900

    BinaryTool*3

--- Assets/RestoreScripts/BinaryTool.cs
+++ Assets/RestoreScripts/BinaryTool.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Security;
+using System.Text;
 using IFix.Core;
 using MoleMole;
 using MoleMole.Config;
@@ -62,6 +63,17 @@ public static class BinaryTool // TypeDefIndex: 14767
         }
         return flag;
     }// 0x000000018094B410-0x000000018094B5F0
+    public static bool FromBinary(ByteArray byteArray, out byte val, int threadFlag = 0 /* Metadata: 0x00AEBED4 */, bool useObjectPool = false /* Metadata: 0x00AEBED8 */)
+    {
+        bool flag = false;
+        if (BinaryTool.CheckValid(byteArray))
+        {
+            val = (byte)byteArray.pos;
+            return byteArray.movePosition(1);
+        }
+        val = 0;
+        return flag;
+    }
     public static bool FromBinary(ByteArray byteArray, out int val, int threadFlag = 0 /* Metadata: 0x00AEBED4 */, bool useObjectPool = false /* Metadata: 0x00AEBED8 */)
     {
         bool flag = false;
@@ -69,6 +81,7 @@ public static class BinaryTool // TypeDefIndex: 14767
         {
             flag = FromBinary(byteArray, out uint vala, threadFlag);
             val = (int)(-(vala & 1) ^ (vala >> 1));
+            return flag;
         }
         val = 0;
         return flag;
@@ -80,8 +93,57 @@ public static class BinaryTool // TypeDefIndex: 14767
         {
             flag = FromBinary(byteArray, _encode[threadFlag]);
             val = (uint)_encode[threadFlag].ToNumber();
+            return flag;
         }
         val = 0;
+        return flag;
+    }
+    public static bool FromBinary(ByteArray byteArray, out string val, int threadFlag = 0 /* Metadata: 0x00AEBED4 */, bool useObjectPool = false /* Metadata: 0x00AEBED8 */)
+    {
+        bool flag = false;
+        if (BinaryTool.CheckValid(byteArray))
+        {
+            flag = FromBinary(byteArray, out uint val1, threadFlag);
+            if (val1 != 0)
+            {
+                if (threadFlag != 0)
+                {
+                    val = Encoding.UTF8.GetString(byteArray.bytes, byteArray.pos, (int)val1);
+                }
+                else
+                {
+                    val = Encoding.UTF8.GetString(byteArray.bytes, byteArray.pos, (int)val1);
+                    UniqueString.Intern(val);
+                }
+            }
+            else
+            {
+                val = String.Intern("");
+            }
+            return byteArray.movePosition((int)val1);
+        }
+        val = null;
+        return flag;
+    }
+    public static bool FromBinary(ByteArray byteArray, out ExistFlag val, int threadFlag = 0 /* Metadata: 0x00AEBED4 */, bool useObjectPool = false /* Metadata: 0x00AEBED8 */)
+    {
+        bool flag = false;
+        if (BinaryTool.CheckValid(byteArray))
+        {
+            FromBinary(byteArray, out byte vala, threadFlag);
+            val = new ExistFlag();
+            val.bytes = new byte[vala];
+            if (val != null)
+            {
+                for (int i = 0; i < vala; i++)
+                {
+                    byteArray.bytes[i++] = byteArray.bytes[i + byteArray.pos];
+                }
+            }
+            flag = byteArray.movePosition(vala);
+            return flag;
+        }
+        val = null;
         return flag;
     }
     public static bool FromBinary(ByteArray byteArray, out EmbeddedTextMapConfig data, int threadFlag = 0 /* Metadata: 0x00AEBED4 */, bool useObjectPool = false /* Metadata: 0x00AEBED8 */)
@@ -95,6 +157,34 @@ public static class BinaryTool // TypeDefIndex: 14767
             data = new EmbeddedTextMapConfig();
         }
         return data.FromBinary(byteArray, threadFlag, useObjectPool);
+    }
+    public static bool FromBinary(ByteArray byteArray, out TextMapConfig data, int threadFlag = 0 /* Metadata: 0x00AEBED4 */, bool useObjectPool = false /* Metadata: 0x00AEBED8 */)
+    {
+        bool flag = false;
+        data = new TextMapConfig();
+        if (FromBinary(byteArray, out ExistFlag val))
+        {
+            if (!FieldTool.Exist(val, 0) || BinaryTool.FromBinary(byteArray, out data.textMapId))
+            {
+                if (!FieldTool.Exist(val, 1) || BinaryTool.FromBinary(byteArray, out data.textMapContent))
+                {
+                    flag = true;
+                }
+                else
+                {
+                    SuperDebug.LogError("FromBinary for: textMapContent  fails!");
+                }
+            }
+            else
+            {
+                SuperDebug.LogError("FromBinary for: textMapId  fails!");
+            }
+        }
+        else
+        {
+            SuperDebug.LogError("FromBinary for memberExist in TextMapConfig fails!");
+        }
+        return flag;
     }
 }
 
