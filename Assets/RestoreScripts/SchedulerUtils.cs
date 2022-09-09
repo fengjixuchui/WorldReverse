259791fa19cbcc271f0a15a4f242cc77ade7dbf8
ShiyumeMeguri <102138304+ShiyumeMeguri@users.noreply.github.com>
2022-09-09 15:29:58 +0900

    SchedulerUtils-All

---
+++ Assets/RestoreScripts/SchedulerUtils.cs
@@ -0,0 +1,111 @@
+﻿/*
+ * Generated code file by Il2CppInspector - http://www.djkaty.com - https://github.com/djkaty
+ */
+
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using IFix.Core;
+
+// Image 59: Assembly-CSharp-firstpass.dll - Assembly: Assembly-CSharp-firstpass, Version=1.0.2.0, Culture=neutral, PublicKeyToken=null - Types 6705-10699
+
+namespace miHoYoThread
+{
+    public static class SchedulerUtils // TypeDefIndex: 9052
+    {
+        // Methods
+        // [XID] // 0x000000018974A210-0x000000018974A230
+        public static void ScheduleEntry(SchedulerMgr schedulerMgr, SchedulerType schedulerType, bool lockScene)
+        {
+            if (schedulerMgr != null)
+            {
+                schedulerMgr.ScheduleEntry((int)schedulerType, lockScene);
+                schedulerMgr.ScheduleMainThreadEntry((int)schedulerType);
+            }
+        } // 0x0000000187639890-0x0000000187639970
+          // [XID] // 0x0000000189751560-0x0000000189751580
+        public static void SchedulerWaitFlush(SchedulerMgr schedulerMgr, SchedulerType schedulerType, bool lockScene)
+        {
+            if (schedulerMgr != null)
+            {
+                schedulerMgr.ScheduleWaitAllFinished((int)schedulerType);
+                schedulerMgr.ScheduleFlush((int)schedulerType, lockScene);
+            }
+        } // 0x0000000187639EC0-0x0000000187639FA0
+          // [XID] // 0x000000018983F6B0-0x000000018983F6D0
+        public static void SchedulePause(SchedulerMgr schedulerMgr, SchedulerType schedulerType)
+        {
+            if (schedulerMgr != null)
+            {
+                schedulerMgr.SchedulePause((int)schedulerType);
+            }
+        } // 0x0000000187639C70-0x0000000187639D30
+          // [XID] // 0x0000000189760690-0x00000001897606B0
+        public static void ScheduleResume(SchedulerMgr schedulerMgr, SchedulerType schedulerType)
+        {
+            if (schedulerMgr != null)
+            {
+                schedulerMgr.ScheduleResume((int)schedulerType);
+            }
+        } // 0x0000000187639D30-0x0000000187639DF0
+          // [XID] // 0x0000000189767A30-0x0000000189767A50
+        public static void ScheduleGlobalEntry(SchedulerGlobalMgr schedulerMgr, SchedulerGlobalType schedulerType)
+        {
+            if (schedulerMgr != null)
+            {
+                schedulerMgr.ScheduleEntry((int)schedulerType, false);
+                schedulerMgr.ScheduleMainThreadEntry((int)schedulerType);
+            }
+        } // 0x0000000187639A20-0x0000000187639AF0
+          // [XID] // 0x000000018976F0A0-0x000000018976F0C0
+        public static void SchedulerGlobalWaitFlush(SchedulerGlobalMgr schedulerMgr, SchedulerGlobalType schedulerType)
+        {
+            if (schedulerMgr != null)
+            {
+                schedulerMgr.ScheduleWaitAllFinished((int)schedulerType);
+                schedulerMgr.ScheduleFlush((int)schedulerType, false);
+            }
+        } // 0x0000000187639DF0-0x0000000187639EC0
+          // [XID] // 0x0000000189776B40-0x0000000189776B60
+        public static void ScheduleGlobalPause(SchedulerGlobalMgr schedulerMgr, SchedulerGlobalType schedulerType)
+        {
+            if (schedulerMgr != null)
+            {
+                schedulerMgr.SchedulePause((int)schedulerType);
+            }
+        } // 0x0000000187639AF0-0x0000000187639BB0
+          // [XID] // 0x0000000189942910-0x0000000189942930
+        public static void ScheduleGlobalResume(SchedulerGlobalMgr schedulerMgr, SchedulerGlobalType schedulerType)
+        {
+            if (schedulerMgr != null)
+            {
+                schedulerMgr.ScheduleResume((int)schedulerType);
+            }
+        } // 0x0000000187639BB0-0x0000000187639C70
+          // [XID] // 0x0000000189951C50-0x0000000189951C70
+        public static void ScheduleGlobalBackground(SchedulerGlobalMgr schedulerMgr)
+        {
+            if (schedulerMgr != null)
+            {
+                schedulerMgr.ScheduleBackground();
+            }
+        } // 0x0000000187639970-0x0000000187639A20
+          // [XID] // 0x000000018978CF60-0x000000018978CF80
+        public static void ScheduleBackgroundPause(SchedulerMgr schedulerMgr)
+        {
+            if (schedulerMgr != null)
+            {
+                schedulerMgr.ScheduleBackgroundPause();
+            }
+        } // 0x0000000187639730-0x00000001876397E0
+          // [XID] // 0x00000001897943D0-0x00000001897943F0
+        public static void ScheduleBackgroundResume(SchedulerMgr schedulerMgr)
+        {
+            if (schedulerMgr != null)
+            {
+                schedulerMgr.ScheduleBackgroundResume();
+            }
+        } // 0x00000001876397E0-0x0000000187639890
+    }
+}
